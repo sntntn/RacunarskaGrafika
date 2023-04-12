b2c2b370578b7f225d6e0424ebd8a26282b84cd0
sntntn <skolskiacc@gmail.com>
2023-04-12 10:20:15 +0200

    nesto nesto

--- src/main.cpp
+++ src/main.cpp
@@ -15,9 +15,9 @@
 #include <learnopengl/model.h>
 
 #include <iostream>
-//ovde smo dodali komentar
+//ovde smo  komentar
 void framebuffer_size_callback(GLFWwindow *window, int width, int height);
-
+///jopjop
 void mouse_callback(GLFWwindow *window, double xpos, double ypos);
 
 void scroll_callback(GLFWwindow *window, double xoffset, double yoffset);
